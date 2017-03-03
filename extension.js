8371053e586b998753841ba16a30d95424d48379
Jongbin Jung <jongbin@stanford.edu>
2017-03-02 22:52:52 -0800

    Port to extend PanelMenu class with Mainloop

--- extension.js
+++ extension.js
@@ -1,41 +1,71 @@
 const St = imports.gi.St;
 const Lang = imports.lang;
 const PanelMenu = imports.ui.panelMenu;
-const PopupMenu = imports.ui.popupMenu;
 const Main = imports.ui.main;
-const Tweener = imports.ui.tweener;
-const Util = imports.misc.util;
+const Mainloop = imports.mainloop;
 const GLib = imports.gi.GLib;
 const Gio = imports.gi.Gio;
-const Shell = imports.gi.Shell;
-const ExtensionUtils = imports.misc.extensionUtils;
-const Me = ExtensionUtils.getCurrentExtension();
-const EXTENSIONDIR = Me.dir.get_path();
-const Mainloop = imports.mainloop;
-
-let button, icon, icon_path, text;
 
 // Kerberos related variables
 let klist  // list of things returned from klist
+
+const IconOn = 'stanford_auth_on';
+const IconOff = 'stanford_auth_off';
 
 const KerberosIndicator = new Lang.Class({
     Name: 'KerberosIndicator',
     Extends: PanelMenu.Button,
 
     _init: function () {
+        this.klist = "";
 		this.parent(null, 'ska');
-        let that = this;
-        let gicon = Gio.icon_new_for_string(Me.path + '/icons/stanford_auth_off.svg');
-        let icon = new St.Icon({
-            gicon: gicon,
+        this.gicon = Gio.icon_new_for_string(IconOff);
+        this.icon = new St.Icon({
+            gicon: this.gicon,
             style_class: 'system-status-icon'
         });
-        this.actor.add_actor(icon);
-        //this.actor.connect('activate', Lang.bind(this, _toggleStatus))
+        this.actor.add_actor(this.icon);
+        this.actor.connect('button-press-event', Lang.bind(this, this._toggleStatus));
+        this._isAuthenticated();
     },
-});
 
-var control = {
+    _toggleStatus: function () {
+        if (this._isAuthenticated()) {
+            this._copy2FA();
+        } else {
+            this._kinit();
+        }
+
+        this._isAuthenticated();
+    },
+
+    _isAuthenticated: function () {
+        this._timeout = Mainloop.timeout_add_seconds(60,
+            Lang.bind(this, this._isAuthenticated));
+        this.klist = GLib.spawn_command_line_sync("klist");
+
+        // check if Kerberos authentication is present
+        if (this.klist[0] == true && this.klist[1] == "") {
+            this.icon.gicon = Gio.icon_new_for_string(IconOff);
+            return false;
+        }
+
+        if (this.klist[0] == true && this.klist[2] == "") {
+            dates = String.fromCharCode.apply(null,
+                this.klist[1]).match(/(\d+\/\d+\/\d+ \d+:\d+:\d+)/g);
+            if (Date.parse(dates[1]) < Date.now()) {
+                this.icon.gicon = Gio.icon_new_for_string(IconOff);
+                return false;
+            } else {
+                this.icon.gicon = Gio.icon_new_for_string(IconOn);
+                return true;
+            }
+        }
+
+        this.icon.gicon = Gio.icon_new_for_string(IconOff);
+        return false;
+    },
+
     _spawn_async: function(cmd, e) {
         try {
             GLib.spawn_command_line_async(cmd, e);
@@ -43,116 +73,28 @@ var control = {
             throw e;
         }
     },
-
-    init: function() {
+    _kinit: function() {
         this._spawn_async("gnome-terminal --command kinit", null);
     },
-
-    copy2FA: function() {
+    _copy2FA: function() {
         // TODO(jongbin): Better way to find 2fa
         this._spawn_async("/home/jongbin/repos/scripts/2fa stan", null);
     }
-};
-
-function _isAuthenticated() {
-    klist = GLib.spawn_command_line_sync("klist");
-
-    // check if Kerberos authentication is present
-    if (klist[0] == true && klist[1] == "") {
-        return false;
-    }
-
-    if (klist[0] == true && klist[2] == "") {
-        dates = String.fromCharCode.apply(null,
-            klist[1]).match(/(\d+\/\d+\/\d+ \d+:\d+:\d+)/g);
-        if (Date.parse(dates[1]) < Date.now()) {
-            return false;
-        } else {
-            return true;
-        }
-    }
-
-    return false;
-}
-
-function _hideMessage() {
-    Main.uiGroup.remove_actor(text);
-    text = null;
-}
-
-function _toggleStatus() {
-    if (_isAuthenticated() == false) {
-        control.init();
-    }
-
-    control.copy2FA();
-
-    let dates = String.fromCharCode.apply(null,
-        klist[1]).match(/(\d+\/\d+\/\d+ \d+:\d+:\d+)/g);
-
-    label = 'Ticket expires: ' + dates[1];
-
-    if (!text) {
-        text = new St.Label({ style_class: 'helloworld-label', text: label });
-        Main.uiGroup.add_actor(text);
-    }
-
-    text.opacity = 255;
-
-    let monitor = Main.layoutManager.primaryMonitor;
-
-    text.set_position(monitor.x + Math.floor(monitor.width / 2 - text.width / 2),
-                      monitor.y + Math.floor(monitor.height / 2 - text.height / 2));
-
-    Tweener.addTween(text, {
-        opacity: 0,
-        time: 4,
-        transition: 'easeOutQuad',
-        onComplete: _hideMessage
-    });
-
-    _updateStatus();
-}
-
-function _updateStatus() {
-    if (_isAuthenticated()) {
-        icon_path = Me.path + '/icons/stanford_auth_on.svg';
-    } else {
-        icon_path = Me.path + '/icons/stanford_auth_off.svg';
-    }
-
-    icon.gicon = Gio.icon_new_for_string(icon_path);
-}
+});
 
 let skaMenu;
 
-function init() {
-    //button = new St.Bin({
-        //style_class: 'panel-button',
-        //reactive: true,
-        //can_focus: true,
-        //x_fill: false,
-        //y_fill: true,
-        //track_hover: true
-    //});
-
-    //icon = new St.Icon({
-        //style_class: 'system-status-icon'
-    //});
-
-    //_updateStatus();
-
-    //button.set_child(icon);
-    //button.connect('button-press-event', _toggleStatus);
+function init(extensionMeta) {
+    // add icons path to the theme search path
+    let theme = imports.gi.Gtk.IconTheme.get_default();
+    theme.append_search_path(extensionMeta.path + "/icons");
 }
 
 function enable() {
-    //Main.panel._rightBox.insert_child_at_index(button, 0);
     skaMenu = new KerberosIndicator;
     Main.panel.addToStatusArea('ska-menu', skaMenu);
 }
 
 function disable() {
-    //Main.panel._rightBox.remove_child(button);
     skaMenu.destroy();
 }
